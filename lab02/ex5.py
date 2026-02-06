44742ba712f0bf527f591d1c6168acb227235768
gianna-k0520 <gianna.kong@ucalgary.ca>
2026-02-06 10:52:28 -0700

    Finished the graphs for example 5

--- lab02/ex5.py
+++ lab02/ex5.py
@@ -3,6 +3,7 @@ from random import randint
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
 
 def linear_search(data, target):
     length = len(data)
@@ -16,45 +17,37 @@ def binary_search(data, target):
     low = 0
     while low <= high:
         mid = (high + low) // 2
-
-        if data[mid] == target:
-            return mid
-        
-        if data[mid] < target:
+        if data[mid] > target:
+            high = mid - 1   
+        elif data[mid] < target:
             low = mid + 1
-        
         else:
-            high = mid - 1
-
+            return mid
     return -1
 
 run_numbers = [1000, 2000, 4000, 8000, 16000, 32000]
-# testing = binary_search(sorted_data, sorted_data[randint(0, 5)])
-# print(f"{sorted_data[testing]}")
 repeat_runs = 1000
 number_runs = 100
+lists = [list(range(n + 1)) for n in run_numbers]
 
 time_linear_average_all = []
 time_linear_average = 0
 for i in range(6):
-    time_linear = timeit.repeat(lambda: linear_search(list(range(run_numbers[i])), randint(0, run_numbers[i])), repeat=repeat_runs, number=number_runs)
-    
-    for j in range(len(time_linear) - 1):
-        time_linear_average += time_linear[j]
-    
-    time_linear_average_all.append(time_linear_average / 1000)
+    time_linear = timeit.repeat(lambda: linear_search(lists[i], randint(0, run_numbers[i])), repeat=repeat_runs, number=number_runs)
+    time_linear_average = sum(time_linear) / len(time_linear)
+    time_linear_average_all.append(time_linear_average)
     print(f"Time for linear average of {run_numbers[i]} data size is: {time_linear_average}")
     time_linear_average = 0
 
 y = np.array(time_linear_average_all)
 x = np.array(run_numbers)
 
-x_dense_grid = np.linspace(min(x), max(x), 200)
+x_dense_grid_linear = np.linspace(min(x), max(x), 200)
 linear_interp = interp1d(x, y, kind = "linear")
-linear_y = linear_interp(x_dense_grid)
+linear_y = linear_interp(x_dense_grid_linear)
 
 plt.scatter(x, y)
-plt.plot(x_dense_grid, linear_y, label = "Linear Interpolation", linestyle = "--")
+plt.plot(x_dense_grid_linear, linear_y, label = "Linear Interpolation", linestyle = "--")
 plt.legend()
 plt.title("Linear Search Interpolation Graph")
 plt.xlabel("Number of averages (1000-32000 pieces of data)")
@@ -66,17 +59,29 @@ print("\n", "-" * 100, "\n")
 time_binary_average_all = []
 time_binary_average = 0
 for i in range(6):
-    time_binary = timeit.repeat(lambda: binary_search(list(range(run_numbers[i])), randint(0, run_numbers[i])), repeat=repeat_runs, number=number_runs)
+    time_binary = timeit.repeat(lambda: binary_search(lists[i], randint(0, run_numbers[i])), repeat=repeat_runs, number=number_runs)
     
-    for j in range(len(time_binary) - 1):
-        time_binary_average += time_binary[j]
-    
-    print(f"The average time taken for the binary search to find a point in an array of {run_numbers[i]} is: {time_binary_average / 1000}")
-    time_binary_average_all.append(time_binary_average / 1000)
-    time_linear_average = 0
+    time_binary_average = sum(time_binary) / len(time_binary)
+    time_binary_average_all.append(time_binary_average)
+    print(f"The average time taken for the binary search to find a point in an array of {run_numbers[i]} is: {time_binary_average}")
+    time_binary_average = 0
 
-# plt.scatter(range(1000), time_binary)
-# plt.title("Binary Graph")
-# plt.xlabel("Number of inputs")
-# plt.ylabel("Time (seconds)")
-# plt.show()
+def log_func(x, a, b):
+    return a * np.log(x) + b
+
+x_binary = np.array(run_numbers)
+y_binary = np.array(time_binary_average_all)
+params, covariance = curve_fit(log_func, x_binary, y_binary)
+a_optimal, b_optimal = params
+x_dense_binary = np.linspace(min(run_numbers), max(run_numbers), 200)
+y_fit_binary = log_func(x_dense_binary, a_optimal, b_optimal)
+
+plt.scatter(x_binary, y_binary)
+plt.plot(x_dense_binary, y_fit_binary, label = "Binary Interpolation", linestyle = "--")
+plt.legend()
+plt.title("Binary Search Interpolation Graph")
+plt.xlabel("Number of averages (1000-32000 pieces of data)")
+plt.ylabel("Time (seconds)")
+plt.show()
+
+# 4: 
